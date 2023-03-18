1a02123ef1838f307eae568421a7f67f1726a9e1
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2023-03-18 15:06:19 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,7 +98,7 @@ TW_IGNORE_MAJOR_AXIS_0 := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 RECOVERY_TOUCHSCREEN_SWAP_XY := true
 RECOVERY_TOUCHSCREEN_FLIP_Y := true
-TW_ROTATION := 180
+TW_ROTATION := 0
 
 # Show build time on the splash screen
 TW_DEVICE_VERSION=$(shell date '+%Y%m%d') by depesh1977
